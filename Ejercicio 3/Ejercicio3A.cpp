01fdc6aefb7fa60e21462312b0e794374ce48135
Gmongtor <gmongtor@myuax.com>
2023-12-18 10:01:57 +0100

    3A

--- Ejercicio 3/Ejercicio3A.cpp
+++ Ejercicio 3/Ejercicio3A.cpp
@@ -2,5 +2,15 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-
+    if (argc != 2) {
+        printf("Uso: %s <nombre_del_archivo>\n", argv[0]);
+        return 1;
+    }
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL) {
+        printf("No se pudo abrir el archivo %s\n", argv[1]);
+        return 1;
+    }
+    fclose(file);
+    return 0;
 }
