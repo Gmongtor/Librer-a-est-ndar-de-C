df18433c6f766a1c53d381a2dfa484410fa88bae
Gmongtor <gmongtor@myuax.com>
2023-12-18 09:55:46 +0100

    2b

--- Ejercicio 2/Ejercicio2B.cpp
+++ Ejercicio 2/Ejercicio2B.cpp
@@ -1,5 +1,9 @@
 #include <cstring>
 #include <cstdio>
 int main() {
+    char str[100];
+    printf("Ingrese una cadena de caracteres: ");
+    fgets(str, sizeof(str), stdin);
+    str[strcspn(str, "\n")] = 0;
 
 }
