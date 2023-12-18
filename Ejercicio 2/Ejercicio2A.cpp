0ed2a7c117096b50327e460ed133109a44910ebd
Gmongtor <gmongtor@myuax.com>
2023-12-18 09:48:34 +0100

    2A

--- Ejercicio 2/Ejercicio2A.cpp
+++ Ejercicio 2/Ejercicio2A.cpp
@@ -6,5 +6,7 @@ int main() {
     char str1[] = "Hola me llamo Juan";
     char str2[] = "Hola mundo"
     char str3[] = "Hola me llamo Juan";
+    int comparison1 = strcmp(str1, str2);
+    int comparison2 = strcmp(str1, str3);
 
 }
