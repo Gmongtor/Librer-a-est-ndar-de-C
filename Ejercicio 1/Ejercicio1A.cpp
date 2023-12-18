27ce17fa9c8381ca661023da14fddbdf2174d866
Gmongtor <gmongtor@myuax.com>
2023-12-18 09:09:42 +0100

    1A

--- Ejercicio 1/Ejercicio1A.cpp
+++ Ejercicio 1/Ejercicio1A.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <cstdlib>
-int main(){
+int main() {
     int numeros[] = {-10, -5, 0, 5, 10};
 
     int cantidadNumeros = sizeof(numeros) / sizeof(numeros[0]);
 
-    for(int i = 0; i < cantidadNumeros; i++){
-        std::cout << numeros[i] << std::endl;
+    for (int i = 0; i < cantidadNumeros; ++i) {
+        int num = numeros[i];
+        int absNum = abs(num);
+        std::cout << "El valor absoluto de " << num << " es " << absNum << std::endl;
     }
 }
