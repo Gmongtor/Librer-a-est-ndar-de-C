7376f7648de3b955e66b9c45679302f1cd074814
Gmongtor <gmongtor@myuax.com>
2023-12-18 10:04:26 +0100

    3B

--- Ejercicio 3/Ejercicio3B.cpp
+++ Ejercicio 3/Ejercicio3B.cpp
@@ -5,6 +5,12 @@ int main() {
         printf("El archivo no se pudo abrir para escritura.\n");
         return 1;
     }
+
     fprintf(file, "Hola, mundo!\n");
     fclose(file);
 
+    file = fopen("file.txt", "r");
+    if (file == NULL) {
+        printf("El archivo no se pudo abrir para lectura.\n");
+        return 1;
+    }
