d7b444b6c02bb977d4d9ce0fb1155c93dc395820
Gmongtor <gmongtor@myuax.com>
2023-12-18 09:16:45 +0100

    1B

--- Ejercicio 1/Ejercicio1B.cpp
+++ Ejercicio 1/Ejercicio1B.cpp
@@ -4,9 +4,14 @@
 #include <ctime>
 
 int main() {
+
     printf("Números aleatorios con la primera semilla:\n");
     srand(time(0));
     for (int i = 0; i < 3; ++i) {
         printf("%d ", rand());
     }
+    printf("\n");
+
+    time_t start_time = time(0);
+    printf("Números aleatorios con la segunda semilla:\n");
 }
