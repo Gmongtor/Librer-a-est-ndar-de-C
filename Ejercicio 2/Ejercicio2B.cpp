33ae9bd9365b1f3965d2620c00f428ec00c5bd5a
Gmongtor <gmongtor@myuax.com>
2023-12-18 09:56:06 +0100

    2b

--- Ejercicio 2/Ejercicio2B.cpp
+++ Ejercicio 2/Ejercicio2B.cpp
@@ -5,5 +5,7 @@ int main() {
     printf("Ingrese una cadena de caracteres: ");
     fgets(str, sizeof(str), stdin);
     str[strcspn(str, "\n")] = 0;
-
+    int length = strlen(str);
+    printf("La longitud de '%s' es: %d\n", str, length);
+    return 0;
 }
